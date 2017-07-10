14e0c6c55236397eb9e34da336392947a3aa730a
2ndPerk <tymonek.wl@gmail.com>
2017-07-10 16:45:00 -0300

    Undid last commit because it broke something

--- app/src/main/java/com/acme/a3csci3130/CreateContactAcitivity.java
+++ app/src/main/java/com/acme/a3csci3130/CreateContactAcitivity.java
@@ -6,7 +6,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class CreateContactActivity extends Activity {
+public class CreateContactAcitivity extends Activity {
 
     private Button submitButton;
     private EditText nameField, emailField;
